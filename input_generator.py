fc2374612384ceab192c789439d69a246a8350e4
Emily Perry <arbitrarydragon0@gmail.com>
2024-10-07 20:08:54 -0600

    finished input generation

--- input_generator.py
+++ input_generator.py
@@ -1,9 +1,58 @@
+import random
 import numpy as np
 
+'''
+HELPER FUNCTIONS
+'''
+def thresh(x, row):
+    return x <= -(float(row)/1000.0) + 0.4
+
+
+
+'''
+GENERATION
+'''
 rows = 316
 cols = 316
 
-grid = np.zeros((rows,cols), dtype=int)
+grid = np.zeros((rows,cols))
+secondary_grid = np.full((rows,cols), ' ')
+
+# generate a random number between 0 and 1 for each cell
+for row in range(rows):
+    for col in range(cols):
+        grid[row][col] = random.random()
+
+# see if a cell should become a numbered block
+for row in range(rows):
+    for col in range(cols):
+        if not thresh(grid[row][col], row):
+            secondary_grid[row][col] = '0'
+        else:
+            secondary_grid[row][col] = '1'
+
+# figure out what number each block should be
+for row in range(rows):
+    for col in range(cols):
+        if secondary_grid[row][col] == '1':
+            new_rand_num = random.random()
+            if new_rand_num < 0.016:
+                to_assign_num = '4'
+            elif new_rand_num < 0.064:
+                to_assign_num = '3'
+            elif new_rand_num < 0.144:
+                to_assign_num = '2'
+            elif new_rand_num < 0.256:
+                to_assign_num = '1'
+            elif new_rand_num < 0.4:
+                to_assign_num = '0'
+            else:
+                to_assign_num = 'X'
+            secondary_grid[row][col] = to_assign_num
+        else:
+            secondary_grid[row][col] = '.'
+            
+
 
 '''
 OUTPUT
@@ -14,5 +63,5 @@ with open('generated_input.txt', 'w') as file:
 
     # The next lines each contain exactly c chars. 
     # The same chars are used from the input file, with the 'L' replacing '.' chars where lights were added
-    for row in grid:
+    for row in secondary_grid:
         file.write(''.join(map(str, row)) + '\n')
